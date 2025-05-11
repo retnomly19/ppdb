f68235e9b96c1eb0826d2712fadf9473fc249454
liyan-byte <liyanxxs30@gmail.com>
2025-05-11 08:58:04 +0700

    Create sidebar.php
    by Nurul Latifah

---
+++ patrials/sidebar.php
@@ -0,0 +1,9 @@
+<div class="sidebar">
+    <h4>📁 MENU</h4>
+    <a href="dashboard.php">🏠 Dashboard</a>
+    <a href="biodata.php">📝 Isi Biodata</a>
+    <a href="upload.php">📂 Upload Berkas</a>
+    <a href="submit.php">📤 Submit Pendaftaran</a>
+    <a href="status.php">📋 Status</a>
+    <a href="logout.php" class="text-danger">🚪 Logout</a>
+</div>
