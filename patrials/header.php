29f44a511ddfac17f13910404ff151c14c2d6a42
liyan-byte <liyanxxs30@gmail.com>
2025-05-11 08:49:46 +0700

    Delete patrials directory

--- patrials/header.php
+++
@@ -1,4 +0,0 @@
-<div class="header">
-    <h2>PPDB Online</h2>
-    <a href="logout.php" class="logout-btn">Logout</a>
-</div>
--- patrials/sidebar.php
+++
@@ -1,9 +0,0 @@
-<div class="sidebar">
-    <ul>
-        <li><a href="dashboard.php">Dashboard</a></li>
-        <li><a href="biodata.php">Isi Biodata</a></li>
-        <li><a href="upload.php">Upload Berkas</a></li>
-        <li><a href="submit.php">Submit Pendaftaran</a></li>
-        <li><a href="status.php">Cek Status</a></li>
-    </ul>
-</div>
