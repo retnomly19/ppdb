dbfd2cff2ca42a53659dd46e548f0eb4c94aa5bc
liyan-byte <liyanxxs30@gmail.com>
2025-05-11 08:58:58 +0700

    Create footer.php

---
+++ patrials/footer.php
@@ -0,0 +1,5 @@
+<!-- footer.php -->
+<footer class="text-center mt-5 text-muted" style="margin-left:240px; padding: 20px;">
+    <hr>
+    <p>&copy; <?= date('Y'); ?> PPDB Online - SMA GenZ. All rights reserved.</p>
+</footer>
